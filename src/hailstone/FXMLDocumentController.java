dc00184fe419a1972633590bd38501d8f5a66abd
Solid <Solid@86.50.40.178>
2018-02-25 18:46:36 +0200

    Created a basic layout for GUI.

--- src/hailstone/FXMLDocumentController.java
+++ src/hailstone/FXMLDocumentController.java
@@ -10,27 +10,55 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 
 /**
  *
  * @author Solid
  */
 public class FXMLDocumentController implements Initializable {
-    
+    int inputNumber;
+
     @FXML
-    private Label label;
+    private Button goButton;
+    @FXML
+    private TextField numberInput;
+    @FXML
+    private Button addOneButton;
+    @FXML
+    private Button substractOneButton;
+    @FXML
+    private Button addFiveButton;
+    @FXML
+    private Button subtractFiveButton;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    }
+    @FXML
+    private void addOne(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void substractOne(ActionEvent event) {
+    }
+
+    @FXML
+    private void addFive(ActionEvent event) {
+    }
+
+    @FXML
+    private void substractFive(ActionEvent event) {
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Hailstone hail = new Hailstone();
+        inputNumber = 1;
+        numberInput.setText(Integer.toString(inputNumber));
     }
-    
-    
 }
